8ffee03b96b7ec8ad55b210b69f2b73b280e572a
smokeystover <smokeystover@walla.com>
2016-10-27 09:00:27 +0300

    added new variables

--- giddyup.c
+++ giddyup.c
@@ -1,7 +1,13 @@
 #include <stdio.h>
 int main()
+ /* hey this remark is just a teminder how to comment on code*/
 {
+int i,j;
+double x,y;
+x=6;
+y=x+5;
+
     printf( "I am alive!  Beware.\n" );
     getchar();
-    return 0;
+   
 }
